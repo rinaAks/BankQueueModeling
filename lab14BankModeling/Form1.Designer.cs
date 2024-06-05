7cbfa8b84ed2aafe2f6c1827a6f8bf7adb0d2673
rinaAks <128968330+rinaAks@users.noreply.github.com>
2024-06-05 14:41:32 +0700

    Add project files.

---
+++ lab14BankModeling/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace lab14BankModeling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbServingClients = new System.Windows.Forms.Label();
+            this.lbQueueClients = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numOperators = new System.Windows.Forms.NumericUpDown();
+            this.btStop = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOperators)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbServingClients);
+            this.panel1.Controls.Add(this.lbQueueClients);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.numOperators);
+            this.panel1.Controls.Add(this.btStop);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1022, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbServingClients
+            // 
+            this.lbServingClients.AutoSize = true;
+            this.lbServingClients.Location = new System.Drawing.Point(251, 59);
+            this.lbServingClients.Name = "lbServingClients";
+            this.lbServingClients.Size = new System.Drawing.Size(14, 16);
+            this.lbServingClients.TabIndex = 14;
+            this.lbServingClients.Text = "0";
+            // 
+            // lbQueueClients
+            // 
+            this.lbQueueClients.AutoSize = true;
+            this.lbQueueClients.Location = new System.Drawing.Point(251, 22);
+            this.lbQueueClients.Name = "lbQueueClients";
+            this.lbQueueClients.Size = new System.Drawing.Size(14, 16);
+            this.lbQueueClients.TabIndex = 13;
+            this.lbQueueClients.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(385, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Количество операторов";
+            // 
+            // numOperators
+            // 
+            this.numOperators.Location = new System.Drawing.Point(433, 52);
+            this.numOperators.Name = "numOperators";
+            this.numOperators.Size = new System.Drawing.Size(72, 22);
+            this.numOperators.TabIndex = 11;
+            this.numOperators.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(830, 40);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(96, 23);
+            this.btStop.TabIndex = 8;
+            this.btStop.Text = "Стоп";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Клиентов на обслуживании:";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(678, 40);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(114, 23);
+            this.btStart.TabIndex = 5;
+            this.btStart.Text = "Старт";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Клиентов в очереди:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 100);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1022, 515);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 615);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOperators)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Label lbServingClients;
+        private System.Windows.Forms.Label lbQueueClients;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numOperators;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ lab14BankModeling/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab14BankModeling
+{
+    // Агенты: Клиенты (Clients), Очередь (Queue), Операторы (Employees), Банк - управляющий агент
+    public partial class Form1 : Form
+    {
+        bool buttonValue;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonValue = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int operatorsN, queueClientsN, servingClientsN;
+        double nextClientTime, servingEndTime;
+        double time;
+
+        Random rnd = new Random(); //от 0 до 1
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            queueClientsN = 0;
+            servingClientsN = 0;
+            time = 0;
+
+            buttonValue = true;
+
+            operatorsN = (int)numOperators.Value;
+            chart1.ChartAreas[0].Axes[1].Maximum = operatorsN + 1;
+
+
+            
+
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nextClientTime = ExpRV(3);
+            if (servingClientsN > 0)
+            {
+                servingEndTime = ExpRV(3 * servingClientsN);
+            }
+            else
+            {
+                servingEndTime = 100;
+            }
+
+            if (nextClientTime < servingEndTime)
+            {
+                if (servingClientsN < operatorsN)
+                {
+                    servingClientsN++;
+                }
+                else
+                {
+                    queueClientsN++;
+                }
+                time += nextClientTime;
+            }
+            else
+            {
+                if (queueClientsN == 0)
+                {
+                    servingClientsN--;
+                }
+                else
+                {
+                    queueClientsN--;
+                }
+                time += servingEndTime;
+            }
+
+            lbQueueClients.Text = queueClientsN.ToString();
+            lbServingClients.Text = servingClientsN.ToString();
+
+            chart1.Series[0].Points.AddXY(time, servingClientsN);
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            buttonValue = false;
+            timer1.Stop();
+        }
+
+
+        double ExpRV(double lambda)
+        {
+            return -Math.Log(rnd.NextDouble()) /lambda;
+        }
+    }
+}
